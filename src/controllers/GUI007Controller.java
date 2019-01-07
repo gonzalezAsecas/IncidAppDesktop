d928fa263af9f3bede1c4730424c279283e8af09
Tesnal <Tesnal@192.168.0.17>
2019-01-07 21:31:39 +0100

    07/01 Casa. Inicio de GUI007 y su controller.

--- src/controllers/GUI007Controller.java
+++ src/controllers/GUI007Controller.java
@@ -5,26 +5,93 @@
  */
 package controllers;
 
-import javabeans.UserBean;
+import factories.LogicFactory;
+import interfaces.iTownHall;
+import java.util.logging.Logger;
+import javabeans.TownHallBean;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
+ * FXML Controller class
  *
- * @author Jon Gonzalez
+ * @author Lander Lluvia
  */
 public class GUI007Controller {
 
-    void setStage(Stage logOutStage) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    protected static final Logger LOGGER = Logger.getLogger("incidappdesktop");
+    
+    @FXML
+    private Menu mTownhall;
+    @FXML
+    private Menu mUsers;
+    @FXML
+    private Menu mInformation;
+    @FXML
+    private Menu mLogOut;
+    @FXML
+    private TableColumn<?, ?> tcName;
+    @FXML
+    private TableColumn<?, ?> tcEmail;
+    @FXML
+    private TableColumn<?, ?> tcTelephone;
+    @FXML
+    private Button btnNewTownhall;
+    @FXML
+    private Button btnModifyTownhall;
+    @FXML
+    private Button btnDelete;
+    
+    protected Stage stage;
+    
+    protected iTownHall townHallImpl = LogicFactory.getiTownHall();
+    
+    protected TownHallBean townhall;
 
-    void setUser(UserBean user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setTownHall(TownHallBean townhall) {
+        this.townhall = townhall;
     }
-
-    void initStage(Parent root) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+    
+    public void initStage(Parent root) {
+        LOGGER.info("Initializing GUI007 stage");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Townhalls");
+        stage.setResizable(true);
+        stage.setOnShowing(this::OnShowingHandler);
+        mUsers.setOnAction();
+        mInformation.setOnAction();
+        mLogOut.setOnAction();
+        btnNewTownhall.setOnAction();
+        btnModifyTownhall.setOnAction();
+        btnDelete.setOnAction();
+        //TODO: cargas los datos de la tabla
+        
+    }
+    
+    public void OnShowingHandler(WindowEvent event){
+        LOGGER.info("Beginning OnShowingHandler");
+        mTownhall.setDisable(true);
+        //Set the mnemonics
+        btnNewTownhall.setMnemonicParsing(true);
+        btnNewTownhall.setText("_New Townhall");
+        btnModifyTownhall.setMnemonicParsing(true);
+        btnModifyTownhall.setText("_Modify Townhall");
+        btnDelete.setMnemonicParsing(true);
+        btnDelete.setText("_Delete");
+        //load the all data
+        //loadData(); ¿? ver loadData GUI003Controller
+        LOGGER.info("Ending OnShowingHandler");
     }
     
 }
